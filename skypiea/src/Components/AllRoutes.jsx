5bc46f722beacb84773779dad583a31d3ea3c46b
harry-io <112643733+harry-io@users.noreply.github.com>
2022-12-15 12:52:37 +0530

    day-3 updates

--- skypiea/src/Components/AllRoutes.jsx
+++ skypiea/src/Components/AllRoutes.jsx
@@ -3,6 +3,7 @@ import Home from "../Pages/Home";
 import Hotels from "../Pages/Hotels";
 import Login from "../Pages/Login";
 import Signup from "../Pages/Signup";
+import Payment from "../Pages/Payment";
 
 export default function AllRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function AllRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/hotels" element={<Hotels />} />
+        <Route path="/payment" element={<Payment />} />
         <Route path="/hotels/:id" />
       </Routes>
     </>
