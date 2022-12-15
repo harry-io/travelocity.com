c397db36be33d15fd03d00247ab5607d9ab3264e
harry-io <112643733+harry-io@users.noreply.github.com>
2022-12-15 14:36:07 +0530

    payment page

--- skypiea/src/Components/NavbarB.jsx
+++ skypiea/src/Components/NavbarB.jsx
@@ -1,7 +1,59 @@
 export default function NavbarB() {
   return (
     <>
-      <div>hbchj</div>
+      <div
+        className="navbarB"
+        style={{ backgroundColor: "navy", width: "100%" }}
+      >
+        {/*  */}
+        <div
+          style={{
+            width: "100%",
+            height: "48px",
+            maxWidth: "1247px",
+            margin: "auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "0.8% 0 ",
+          }}
+        >
+          {/* part_a */}
+          <div
+            style={{
+              width: "10%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+              padding: "0.8em",
+            }}
+          >
+            <div style={{ width: "100%" }}>
+              <img
+                style={{ width: "100%", cursor: "pointer" }}
+                src="https://i.ibb.co/7XX7GPJ/skypiea-logo.png"
+                alt="skypiea_logo"
+                onClick={() => alert("home")}
+              />
+            </div>
+          </div>
+          {/* part_b */}
+          <div
+            style={{
+              width: "15%",
+              display: "flex",
+              justifyContent: "space-evenly",
+              gap: "3%",
+              color: "white",
+            }}
+          >
+            {/*  */}
+            <p style={{ fontSize: "1vw" }}>Hello, name</p>
+          </div>
+          {/*  */}
+        </div>
+        {/*  */}
+      </div>
     </>
   );
 }
--- skypiea/src/Pages/Payment.jsx
+++ skypiea/src/Pages/Payment.jsx
@@ -1,9 +1,89 @@
 import NavbarB from "../Components/NavbarB";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faLock } from "@fortawesome/free-solid-svg-icons";
 
 export default function Payment() {
   return (
     <>
       <NavbarB />
+      {/* A */}
+      <div
+        style={{
+          width: "90%",
+          maxWidth: "1247px",
+          margin: "auto",
+          gap: "2%",
+          lineHeight: "1rem",
+          marginBottom: "20px",
+        }}
+      >
+        <h3>Secure booking — only takes 2 minutes!</h3>
+        <div
+          style={{
+            fontSize: "small",
+            display: "flex",
+            alignItems: "center",
+            gap: "1%",
+            color: "green",
+          }}
+        >
+          <FontAwesomeIcon icon={faCheck} />
+          <p>You've picked a winner! This hotel is rated 9.2/10.</p>
+        </div>
+      </div>
+      {/* B */}
+      <div
+        style={{
+          width: "90%",
+          maxWidth: "1247px",
+          margin: "auto",
+          display: "flex",
+          gap: "2%",
+        }}
+      >
+        <div
+          style={{
+            width: "65%",
+            backgroundColor: "white",
+            padding: "0.7% 0",
+          }}
+        >
+          <div
+            style={{
+              width: "95%",
+              margin: "auto",
+
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <div>
+              <FontAwesomeIcon icon={faLock} />
+            </div>
+            <div
+              style={{
+                lineHeight: "0.1rem",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "small",
+                }}
+              >
+                Signed in as
+              </p>
+              <p>Email address</p>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "25%",
+            border: "1px solid black",
+          }}
+        ></div>
+      </div>
     </>
   );
 }
